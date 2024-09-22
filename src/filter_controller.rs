0cf4a2a0fdf75f95402cd8bf77761cada0270354
nng <noah.garkisch@gmail.com>
2024-09-22 21:34:12 +0200

    fix activation and rename prroject

--- src/filter_controller.rs
+++ src/filter_controller.rs
@@ -2,37 +2,46 @@ use rppal::gpio::OutputPin;
 use std::thread::sleep;
 use std::time::Duration;
 
+const RELAY_DELAY_MS: u64 = 50;
+
 pub struct FilterController {
     relay1: OutputPin,
     relay2: OutputPin,
+    running: bool,
 }
 
 impl FilterController {
     pub fn new(relay1: OutputPin, relay2: OutputPin) -> FilterController {
         FilterController {
             relay1,
-            relay2
+            relay2,
+            running: false,
         }
     }
 
-    fn start_filter_process(&mut self) {
-        self.relay1.set_high();
-        sleep(Duration::from_secs(1));
-        self.relay2.set_low();
-        sleep(Duration::from_secs(1));
+    pub fn is_running(&self) -> bool {
+        self.running
+    }
+
+    pub fn start_filter_process(&mut self) {
+        if !self.is_running() {
+            self.relay1.set_low();
+            sleep(Duration::from_millis(RELAY_DELAY_MS));
+            self.relay2.set_low();
+            println!("Relays turned on");
+            sleep(Duration::from_millis(RELAY_DELAY_MS));
+            self.running = true;
+        }
     }
 
     pub fn stop_filter_process(&mut self) {
-        self.relay1.set_low();
-        sleep(Duration::from_secs(1));
-        self.relay2.set_low();
-        sleep(Duration::from_secs(1));
+        if self.is_running() {
+            self.relay1.set_high();
+            sleep(Duration::from_millis(RELAY_DELAY_MS));
+            self.relay2.set_high();
+            println!("Relays turned off");
+            sleep(Duration::from_millis(RELAY_DELAY_MS));
+            self.running = false;
+        }
     }
-
-    pub fn run_filter_process(&mut self) {
-       self.start_filter_process();
-       sleep(Duration::from_secs(10));
-       self.stop_filter_process();
-       sleep(Duration::from_secs(20));
-    }
-} 
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,78 @@
+use ctrlc;
 use rppal::gpio::Gpio;
-use std::time::{Duration, Instant};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread::sleep;
-use ctrlc;
+use std::time::{Duration, Instant};
 mod filter_controller;
+
+const RUN_DURATION_SECS: u64 = 10;
+const PAUSE_DURATION_SECS: u64 = 20;
+const MAX_FILTER_INTERVAL_SECS: u64 = 1800;
 
 fn main() {
     println!("Process started");
-    const RELAY_1_PIN: u8 = 17;
-    const RELAY_2_PIN: u8 = 27;
-    const SENSOR_2_PIN: u8 = 22;
+    const RELAY_1_PIN: u8 = 2;
+    const RELAY_2_PIN: u8 = 17;
+    const SENSOR_PIN: u8 = 22;
 
     let gpio = Gpio::new().expect("Failed to access GPIO");
-    let relay1 = gpio.get(RELAY_1_PIN).expect("Failed to get relay 1").into_output();
-    let relay2 = gpio.get(RELAY_2_PIN).expect("Failed to get relay 2").into_output();
-    let float_sensor = gpio.get(SENSOR_2_PIN).expect("Failed to get sensor 2").into_input_pullup();
+    let mut relay1 = gpio
+        .get(RELAY_1_PIN)
+        .expect("Failed to get relay 1")
+        .into_output();
+    let mut relay2 = gpio
+        .get(RELAY_2_PIN)
+        .expect("Failed to get relay 2")
+        .into_output();
+    let float_sensor = gpio
+        .get(SENSOR_PIN)
+        .expect("Failed to get sensor 2")
+        .into_input_pullup();
 
-    let mut filter_controller = filter_controller::FilterController::new( relay1, relay2); 
+    // ensure relays are off initially
+    relay1.set_high();
+    relay2.set_high();
 
-    let mut last_filter_run = Instant::now();
-
+    let mut filter_controller = filter_controller::FilterController::new(relay1, relay2);
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
 
     ctrlc::set_handler(move || {
         println!("Exiting... Turning off relays");
         r.store(false, Ordering::SeqCst);
-    }).expect("Error setting Ctrl+C handler");
+    })
+    .expect("Error setting Ctrl+C handler");
+
+    filter_controller.start_filter_process();
+    let mut last_filter_run = Instant::now();
 
     while running.load(Ordering::SeqCst) {
-        let now =Instant::now();
-        if float_sensor.is_low() || now.duration_since(last_filter_run) >= Duration::from_secs(1800) {
-            println!("Filter process running");
-            filter_controller.run_filter_process();
+        let now = Instant::now();
+
+        // check if it is time to start filter
+        if !filter_controller.is_running()
+            && now.duration_since(last_filter_run) >= Duration::from_secs(PAUSE_DURATION_SECS)
+        {
+            if float_sensor.is_low()
+                || now.duration_since(last_filter_run)
+                    >= Duration::from_secs(MAX_FILTER_INTERVAL_SECS)
+            {
+                filter_controller.start_filter_process();
+                last_filter_run = Instant::now();
+            }
+        }
+
+        // check if it is time to stop filter
+        if filter_controller.is_running()
+            && now.duration_since(last_filter_run) >= Duration::from_secs(RUN_DURATION_SECS)
+        {
+            filter_controller.stop_filter_process();
             last_filter_run = Instant::now();
         }
+
         sleep(Duration::from_secs(1));
     }
 
     filter_controller.stop_filter_process();
-
 }
